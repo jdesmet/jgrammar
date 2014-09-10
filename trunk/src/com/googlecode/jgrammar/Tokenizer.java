80e1c5ef026fd58d86e2d4957c7d33cf9e06c465
yo.desmet@gmail.com <yo.desmet@gmail.com@3478bc31-dd95-0321-b6a9-072298cfe5b5>
2014-09-10 15:59:41 +0000

    Spelling

--- trunk/src/com/googlecode/jgrammar/Tokenizer.java
+++ trunk/src/com/googlecode/jgrammar/Tokenizer.java
@@ -7,23 +7,28 @@
 package com.googlecode.jgrammar;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.regex.Matcher;
 
 /**
  *
  * @author jdesmet
  */
 class Tokenizer<T> {
-  private List<Pattern> patterns;
-  private List<Rule<T,? extends T>> rules;
+  private final List<Pattern> patterns;
+  private final List<Pattern> ignores;
+  private final List<Rule<T,? extends T>> rules;
 
   public Tokenizer() {
     patterns = new ArrayList<>();
     rules = new ArrayList<>();
+    ignores = new ArrayList<>();
   }
   
-  void ignore(String s) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  void ignore(Pattern pattern) {
+    ignores.add(pattern);
   }
 
   Rule<T,? extends T> add(Rule<T,? extends T> rule) {
@@ -37,7 +42,27 @@ class Tokenizer<T> {
   }
 
   T parse(String string) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //tokens = new LinkedList<>();
+    //tokens.add(new Token(string));
+    List<Token> tokens = tokenize(string);
+    tokens.forEach((t)->{System.out.println("Token "+(t.isTokenized()?t.getPattern().getId():"STRING")+"=\""+t.getString()+"\"");});
+    return null;
+  }
+  
+  List<Token> tokenize(String string) {
+    // Highly inefficient recursive tokenizer
+    List<Token> tokens = new ArrayList<>();
+    for (Pattern p:patterns) {
+      Matcher m = p.matcher(string);
+      if (m.find()) {
+        if (m.start() > 0) tokens.addAll(tokenize(string.substring(0, m.start())));
+        tokens.add(new Token(p,string.substring(m.start(), m.end())));
+        if (m.end() < string.length()) tokens.addAll(tokenize(string.substring(m.end())));
+        return tokens;
+      }
+    }
+    tokens.add(new Token(string));
+    return tokens;
   }
   
 }
